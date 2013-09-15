e808fa561e1660554218df1f257b23a7803cf7b1
Emiliano <jemiliano.cabrera@gmail.com>
2013-09-15 18:58:36 -0500

    Agrego la estructura TCP, faltan funciones

--- src/transport/polv_transport.h
+++ src/transport/polv_transport.h
@@ -10,21 +10,6 @@ enum polv_trans_protocol {
 	UNKNOWN_TRANS
 };
 
-struct tcp {
-	const u_char* src_port;
-	const u_char* dst_port;
-	const u_char* seq;
-	const u_char* ack;
-	const u_char* offset;
-	const u_char* reserved;
-	const u_char* flags;
-	const u_char* window;
-	const u_char* checksum;
-	const u_char* urgent;
-	const u_char* options;
-
-};
-
 struct icmp {
 	const u_char* type;
 	const u_char* code;
